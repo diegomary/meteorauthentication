0dc35cb1bf696378167f0ff8cf9fa9f8a59efd4f
Diego Aldo Burlando <diego@dmm888.com>
2016-12-05 20:42:43 +0000

    Completed implementation of the rememberme flag

--- client/dashboard.js
+++ client/dashboard.js
@@ -1,29 +1,24 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session'
 import './dashboard.html';
 
 Template.dashboard.onCreated(function () {
-  window.onbeforeunload = function (e) {
-    Meteor.logout();
-  };
-
-});
-
-Template.dashboard.onDestroyed(function () {
-
+  let is_ToRemember = Session.get("remember_me");
+  if(!is_ToRemember) window.onbeforeunload = function (e) {Meteor.logout();};
 
 });
 
 
 Template.dashboard.helpers({
-  TODO() {
-
-  },
+  TODO() { },
+  
 });
 
 Template.dashboard.events({
   'click .jq-link-logout'(event, instance) {
     event.preventDefault();
+    Session.set("remember_me", false);
     Meteor.logout();
   },
 
